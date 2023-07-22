a1ad82bd4bdca1d06914a4b850a4b7d84afce845
00shivani <shivanisingh13@icloud.com>
2023-07-21 20:26:31 -0400

    created JS file, added quiz questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+var questions = [
+    {
+        question: "Which of the following are built-in terminal commands used for navigating your computer's file system and completing base-level tasks?",
+        answers: [
+            {text: "mj, skl, ol, ps, ink", correct: false},
+            {text: "cp, rm, touch, ls, pwd", correct: true},
+            {text: "brb, idk, kk, omw, wyd", correct: false},
+            {text: "dvd, dl, feel, create, no", correct: false},
+        ]
+    },
+    {
+        question: "Which of the following is NOT associated with creating or deploying a GitHub repository?",
+        answers: [
+            {text: "changing background color", correct: true},
+            {text: "Pages", correct: false},
+            {text: "HTTPS link / SSH Key", correct: false},
+            {text: "README", correct: false},
+        ]
+    },
+    {
+        question: "What is the correct way to declare a new variable in CSS",
+        answers: [
+            {text: "var rounded-border = 10px;", correct: false},
+            {text: "--rounded-border: 10px;", correct: true},
+            {text: "var(--rounded-border);", correct: false},
+            {text: "var border-radius: 10px;", correct: false},
+        ]
+    },
+    {
+        question: "What is the point of a CSS reset stylesheet?",
+        answers: [
+            {text: "to reset the current CSS stylesheet so you can start it from scratch", correct: false},
+            {text: "to clear the browser's default formatting of HTML elements and remove potential inconsistencies between different browsers", correct: true},
+            {text: "a second CSS file used to a different page on the website so that the original stylesheet doesn't get too crowded", correct: false},
+            {text: "a life reset for when you're having a bad day", correct: false},
+        ]
+    },
+    {
+        question: "Which is the correct way to link your JavaScript file to your project?",
+        answers: [
+            {text: "it must be linked in the head of your HTML file using the <link> tag and 'href' attribute", correct: false},
+            {text: "VSCode automatically knows to link your script file as long as it is properly placed in the assets folder of your project", correct: false},
+            {text: "place a script property at the bottom of your CSS stylesheet and link it using a src attribute", correct: false},
+            {text: "place a script tag with a src attribute linking to your script file at bottom of the body section of your HTML", correct: true},
+        ]
+    },
+    {
+        question: "What is the correct format of a forLoop?",
+        answers: [
+            {text: "(incrementor ; string ; condition)", correct: false},
+            {text: "(loop; boolean; argument)", correct: false},
+            {text: "(initial expression/counter; condition; incrementor)", correct: true},
+            {text: "(function value; variable item; conditionals)", correct: false},
+        ]
+    },
+    {
+        question: "What is the proper format for writing an array in JavaScript?",
+        answers: [
+            {text: "var array = ['item-1', 'item-2', 'item-3'];", correct: true},
+            {text: "var array = {value: 'item-1', property: 'item-2', element: 'item-3'};", correct: false},
+            {text: "var array = 'array';", correct: false},
+            {text: "if [array] = var [array, array-1] it is an array else false;", correct: false},
+        ]
+    },
+    {
+        question: "What method allows you to select all of the specified group of elements?",
+        answers: [
+            {text: "getElementById()", correct: false},
+            {text: "querySelectorAll()", correct: true},
+            {text: "$(<div>)", correct: false},
+            {text: "var = document.select", correct: false},
+        ]
+    },
+    {
+        question: "Which of these events do event listeners NOT listen for?",
+        answers: [
+            {text: "mouse events", correct: false},
+            {text: "keyboard events", correct: false},
+            {text: "window events", correct: false},
+            {text: "wedding events", correct: true},
+        ]
+    },
+    {
+        question: "What is jQuery?",
+        answers: [
+            {text: "a JavaScript library that definitely does not use dollar signs", correct: false},
+            {text: "a JavaScript question generator", correct: false},
+            {text: "a lightweight, 'write less, do more', JavaScript library", correct: true},
+            {text: "a movement that pushes developers to code in vanilla JavaScript", correct: false},
+        ]
+    },
+    
+]
